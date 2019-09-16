015adb319cb8bc79aacb80499a796c4caad2e8bf
Jake Elkins <45544450+jakeelkins@users.noreply.github.com>
2019-09-16 10:48:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_attitudecontrol',
-      version='0.0.6',
-      install_requires=['gym', 'pybullet', 'numpy', 'os']
+      version='0.0.7',
+      install_requires=['gym', 'pybullet', 'numpy']
 )
