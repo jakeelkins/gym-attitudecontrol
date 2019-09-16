2b84ed2ba16a736f9efc931e2f536d71f2de937b
Jake Elkins <45544450+jakeelkins@users.noreply.github.com>
2019-09-16 10:25:47 -0500

    add os dependency

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_attitudecontrol',
       version='0.0.5',
-      install_requires=['gym', 'pybullet', 'numpy']
+      install_requires=['gym', 'pybullet', 'numpy', 'os']
 )
